3543acb48539717986f2712b6e298030b418e941
Mete Sayan <metesayan99@gmail.com>
2023-10-14 15:33:12 +0300

    feat: Users & Roles

---
+++ src/main/java/com/pokemonreview/api/models/Role.java
@@ -0,0 +1,17 @@
+package com.pokemonreview.api.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
