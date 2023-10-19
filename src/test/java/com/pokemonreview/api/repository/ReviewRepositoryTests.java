937fed387b4973705c361885b21aabf5c97cb56b
Mete Sayan <metesayan99@gmail.com>
2023-10-19 23:09:52 +0300

    test(ReviewRepository): Save

---
+++ src/test/java/com/pokemonreview/api/repository/ReviewRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Review;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ReviewRepositoryTests {
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewRepositoryTests(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Test
+    public void ReviewRepository_Save_ReturnsSavedReview() {
+        Review review = Review.builder().title("title").content("content").stars(5).build();
+
+        Review savedReview = reviewRepository.save(review);
+
+        Assertions.assertThat(savedReview).isNotNull();
+        Assertions.assertThat(savedReview.getId()).isGreaterThan(0);
+    }
+}
