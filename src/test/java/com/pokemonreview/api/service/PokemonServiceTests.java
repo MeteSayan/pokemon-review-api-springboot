8bafcd9aa320e16debdd01373fda2af7153c9c38
Mete Sayan <metesayan99@gmail.com>
2023-10-20 16:02:03 +0300

    test(PokemonService): GetPokemonById & UpdatePokemon & DeletePokemonById

--- src/test/java/com/pokemonreview/api/service/PokemonServiceTests.java
+++ src/test/java/com/pokemonreview/api/service/PokemonServiceTests.java
@@ -15,6 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,5 +53,48 @@ public class PokemonServiceTests {
         PokemonResponse savedPokemon = pokemonService.getAllPokemon(1, 10);
 
         Assertions.assertThat(savedPokemon).isNotNull();
+    }
+
+    @Test
+    public void PokemonService_GetPokemonById_ReturnsPokemonDto() {
+        long pokemonId = 1;
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.ofNullable(pokemon));
+
+        PokemonDto savedPokemon = pokemonService.getPokemonById(pokemonId);
+
+        Assertions.assertThat(savedPokemon).isNotNull();
+    }
+
+    @Test
+    public void PokemonService_UpdatePokemon_ReturnsPokemonDto() {
+        long pokemonId = 1;
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+
+        PokemonDto pokemonDto = PokemonDto.builder().name("pikachu").type("electric").build();
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.ofNullable(pokemon));
+        when(pokemonRepository.save(Mockito.any(Pokemon.class))).thenReturn(pokemon);
+
+        PokemonDto savedPokemon = pokemonService.updatePokemon(pokemonDto, pokemonId);
+
+        Assertions.assertThat(savedPokemon).isNotNull();
+    }
+
+    @Test
+    public void PokemonService_DeletePokemonById_ReturnsPokemonDto() {
+        long pokemonId = 1;
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.ofNullable(pokemon));
+
+        assertAll(() -> pokemonService.deletePokemonId(pokemonId));
     }
 }
